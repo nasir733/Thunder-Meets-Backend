56f8ad5afeed2b04cab8e067c64e4ea9e6f3c898
Nasir Iqbal <nasiriqbal@Nasirs-MacBook-Pro.local>
2022-08-25 19:17:41 +0500

    installed gunicorn

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import dj_database_url
 import django_heroku
 
-DEBUG =False
+DEBUG =True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
